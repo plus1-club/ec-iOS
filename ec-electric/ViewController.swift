06817bf300fb7ab359a2460921c083efc482b7bc
iSamTech <samir.azizov@gmail.com>
2019-08-01 17:00:48 +0300

    Enter View Created

--- ec-electric/ViewController.swift
+++ ec-electric/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var round_button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        round_button.layer.cornerRadius = 5.0
+        round_button.layer.masksToBounds = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
