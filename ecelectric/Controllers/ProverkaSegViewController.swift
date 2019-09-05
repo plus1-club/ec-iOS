1b46b4bc3f38006dd74847be8a17bd29c1c824b1
iSamTech <samir.azizov@gmail.com>
2019-09-05 20:25:05 +0300

    "proverka view done"

---
+++ ecelectric/Controllers/ProverkaSegViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SegmentedViewController.swift
+//  ecelectric
+//
+//  Created by Sam on 05/09/2019.
+//  Copyright © 2019 Samir Azizov. All rights reserved.
+//
+
+import UIKit
+
+class ProverkaSegViewController: UIViewController {
+
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        sideMenu()
+    }
+    
+    func sideMenu() {
+        
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            revealViewController().rightViewRevealWidth = 275
+            
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    @IBAction func switchViews(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            firstView.alpha = 1
+            secondView.alpha = 0
+        } else {
+            firstView.alpha = 0
+            secondView.alpha = 1
+        }
+    }
+
+}
