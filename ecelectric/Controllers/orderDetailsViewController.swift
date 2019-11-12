a3f0271a2d227da40eb910b27f2bb55f3508be0e
iSamTech <samir.azizov@gmail.com>
2019-11-12 19:01:29 +0300

    "before api 12.11.2019 19.01"

---
+++ ecelectric/Controllers/orderDetailsViewController.swift
@@ -0,0 +1,30 @@
+ //
+//  orderDetailsViewController.swift
+//  EC-online
+//
+//  Created by Sam on 15/10/2019.
+//  Copyright © 2019 Samir Azizov. All rights reserved.
+//
+
+import UIKit
+
+class orderDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
